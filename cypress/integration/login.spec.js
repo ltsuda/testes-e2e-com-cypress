7af28dfd5a1133993cb6df6333aca3c940ac4351
Leonardo Tsuda <leonardo.tsuda@icloud.com>
2021-09-03 14:30:22 -0300

    add login spec

---
+++ cypress/integration/login.spec.js
@@ -0,0 +1,11 @@
+it("autentica corretamente com um usário cadastrado", () => {
+    cy.intercept("GET", "**/notes").as("getNotes")
+
+    cy.visit("/login")
+    cy.get("#email").type(Cypress.env("USER_EMAIL"))
+    cy.get("#password").type(Cypress.env("USER_PASSWORD"))
+    cy.contains("button", "Login").click()
+
+    cy.wait("@getNotes")
+    cy.contains("h1", "Your Notes").should("be.visible")
+})
